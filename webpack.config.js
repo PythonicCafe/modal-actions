868619a7e7d8ebafe7c24266d2c6bd486a52fbb9
Marcel Marques <marcmatiasdev@gmail.com>
2022-10-18 11:59:11 -0300

    Updating styles folder structure

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = (env, argv) => {
     },
     entry: {
       "modal-actions": "./src/index.js",
-      style: "./modal-actions.scss",
+      style: "./src/assets/scss/modal-actions.scss",
     },
     output: {
       filename: "[name].min.js",
